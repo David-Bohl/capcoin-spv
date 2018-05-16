531b5ae5350c7d0e467a3d76e75e7e5bc341b298
Frederick Erlikh <erlikhf@gmail.com>
2018-05-16 11:13:37 -0400

    fix errors in getting balance

--- controllers/profile.js
+++ controllers/profile.js
@@ -3,43 +3,46 @@ const Redirect = require('../middlewares/redirect');
 const exec = require('child_process').execFile;
 const models = require('../models');
 
+function checkBalance(req, res) {
+  console.log("REQUESTING BALANCE for: " + req.user.public_key);
+  // console.log("address: " + req.body.address);
+  // console.log("amount: " + req.body.amount);
+
+  // A function that will create a process to run the balance.o executable to retrieve the balance from the server
+  var getBalance = function() {
+    console.log("Get Balance called for " + req.user.public_key);
+
+    //The first parameter is the executable (the path starts from project root)
+    //The second parameter is what gets passed to the executable
+    exec('./cpp/bin/balance.o',[req.user.public_key],function(err,data) {
+      //Update profile database model with balance
+      //data.toString() contains the balance
+      console.log(data.toString());
+      models.User.update({balance: data.toString()},{where:{private_key:req.user.private_key}})
+        .then(result => {
+            //console.log(err);
+            console.log(result);
+            //redirect user to profile
+            // res.redirect('/profile');
+            res.render('profile', { user: req.user, success: req.flash('success') });
+          }
+        );
+    });
+  }
+  getBalance();
+}
+
 module.exports = {
   registerRouter() {
     const router = express.Router();
 
     router.get('/', Redirect.ifNotLoggedIn(), this.index);
-    router.post('/', this.checkBalance);
+    router.post('/', checkBalance);
 
     return router;
   },
   index(req, res) {
     // res.render('profile', { user: req.user, success: req.flash('success') });
     checkBalance(req, res);
-  },
-  checkBalance(req, res) {
-    console.log("REQUESTING BALANCE for: " + req.user.public_key);
-    // console.log("address: " + req.body.address);
-    // console.log("amount: " + req.body.amount);
-
-    // A function that will create a process to run the balance.o executable to retrieve the balance from the server
-    var getBalance = function() {
-      console.log("Get Balance called for " + req.body.address);
-
-      //The first parameter is the executable (the path starts from project root)
-      //The second parameter is what gets passed to the executable
-      exec('./cpp/bin/balance.o',[req.user.public_key],function(err,data) {
-        //Update profile database model with balance
-        //data.toString() contains the balance
-        models.User.update({balance: data.toString()},{where:{private_key:req.user.private_key}})
-          .then(result => {
-              //console.log(err);
-              console.log(result);
-              //redirect user to profile
-              res.redirect('/profile');
-            }
-          );
-      });
-    }
-    getBalance();
   },
 };
